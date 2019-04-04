39780c12c5854ea31ae046c9e451fb199b87a3fa
malteserteresa <tess_the_titan@hotmail.com>
2019-04-04 09:54:40 +0200

    Added binomial expansion work and recursive creation of pascals triangle

--- binomial_expansion.py
+++ binomial_expansion.py
@@ -1,13 +1,23 @@
 # Pascals triangle
 
-def pascals_triangle(order):
-    """Prints pascals triangle.
+def pascals_triangle(previous_row):
+    """Prints next line of pascals triangle.
 
     Args:
-        int : The number of rows of pascals triangle to print
+        list : The row preceeding the target row
 
     """
+    row = []
+    mid = get_middle(previous_row)
+    print(mid)
+    if(len(mid) == 2):
+        first_half = recursive_summation(previous_row[:mid[0]+1])
+        return first_half + [previous_row[mid[0]+1]*2] + first_half[::-1]
 
+    return row
+
+def test_even():
+    assert pascals_triangle([1, 3, 3, 1]) == [1, 4, 6, 4, 1]
 
 def recursive_summation(split_row, first = None):
     """Returns the one half of the coefficients of pascals triangle for the next row.
@@ -30,7 +40,7 @@ def test_creates_half_of_next_row():
     assert recursive_summation([1, 6, 15, 20], None) == [1, 7, 21, 35]
 
 def get_middle(_list):
-    """Returns the middle value if lenght of list is odd or values if the length of the list is an even,"""
+    """Returns the middle value if length of list is odd or values if the length of the list is an even,"""
     l = len(_list)
     if (l % 2 == 0):
         m1 = int(l / 2)
