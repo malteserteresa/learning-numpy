1896b88f5bf3986ce17d35a36fa902f7e6694b82
malteserteresa <tess_the_titan@hotmail.com>
2019-04-17 14:48:48 +0200

    Improved notebook on central tendency explanations

--- statistics/representation/central_tendency.py
+++ statistics/representation/central_tendency.py
@@ -19,3 +19,11 @@ def test_mode_string():
     assert mode(strings) == "cat"
 
 
+def right_skew(min, max, n):
+    """Returns an array of numbers between the given limits with numbers skewed to the right."""
+    pers = np.arange(min, max, 1)
+    # Make each of the last 41 elements 5x more likely
+    prob = [1.0] * (len(pers) - 41) + [5.0] * 41
+    # Normalising to 1.0
+    prob /= np.sum(prob)
+    return np.array([ np.random.choice(pers, 1, p=prob)[0] for n in range(0,n) ])
